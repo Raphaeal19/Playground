da574ad671d8bc90d54813a494b780ba0845c842
Ayush Pathak <31178268+Raphaeal19@users.noreply.github.com>
2020-05-26 20:45:18 +0530

    Test code pushed to repository.

---
+++ Its result time/Main.java
@@ -0,0 +1,10 @@
+#include<iostream>
+//typedef std::cout ccout
+//typedef std::cin ccin
+
+int main(){
+  int id, marks;
+  std::cin >> id;
+  std::cin >> marks;
+  std::cout << "Id : " << id << "\nTotalmarks : " << marks;
+}
